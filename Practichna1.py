c4416554fe12692418166c78bd72311a3c7ea88c
MazeliukStanislav <mazeliukstas@gmail.com>
2026-09-06 15:21:44 +0300

    Practichna №1

---
+++ Practichna1.py
@@ -0,0 +1,34 @@
+print("HelloWorld!")
+#Типи змінних:
+num = 123
+text = "Mouse"
+flt_num = 3.14
+active = True
+lst = [1,2,3,4,5]
+tpl = (10, 20)
+dct = {"key" :"value"}
+st = {1,2,3}
+#Виведення типів змінних:
+print(num, type(num))
+print(text, type(text))
+print(flt_num, type(flt_num))
+print(active, type(active))
+print(lst, type(lst))
+print(tpl, type(tpl))
+print(dct, type(dct))
+print(st, type(st))
+print(25 * "-")
+# Оператори
+a = 17
+b = 16
+
+print("Додавання:", (a + b))   # Додавання
+print("Віднімання:", (a - b))   # Віднімання
+print("Множення:", (a * b))   # Множення
+print("Ділення:", (a / b))   # Ділення
+print("Цілочисельне ділення:", (a // b))  # Цілочисельне ділення
+print("Остача від ділення: ", (a % b))   # Остача від ділення
+print("Піднесення до степеня: ", (a ** b))  # Піднесення до степеня
+
+c = 5          # Присвоєння
+print(c)
